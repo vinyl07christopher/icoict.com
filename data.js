5244603bc9f111794afff48fe5fe8dccabae8b6f
vinyl07christopher <86916576+vinyl07christopher@users.noreply.github.com>
2023-08-19 15:40:51 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+
+
+
+// data.js
+var importantDates = [
+  {
+    event: "Full Paper Submission Deadline",
+    date: "5 December, 2023"
+  },
+  {
+    event: "Notification of Revision/Reject",
+    date: "8 January, 2024"
+  },
+  {
+    event: "Acceptance Intimation",
+    date: "10 February, 2024"
+  },
+  {
+    event: "Registration Deadline",
+    date: "12 March, 2024"
+  },
+  {
+    event: "Date of Conference",
+    date: "16-17, April 2024"
+  }
+];
